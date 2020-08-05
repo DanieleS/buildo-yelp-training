47ca192d7ec052d338416609fa4dbe26e3d6440d
Davide Specchia <davide.specchia@buildo.io>
2020-08-05 17:39:14 +0200

    Display api result in the view

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,25 @@
 import React from "react";
 import { app } from "./App.treat";
+import { useQuery } from "avenger/lib/react";
+import { fold } from "avenger/lib/QueryResult";
+import { businessListQuery } from "../../queries/queries";
+import { pipe } from "fp-ts/lib/function";
+import BusinessList from "../BusinessList/BusinessList";
 
 function App() {
-  return <div className={app}>Yelp clone</div>;
+  const businessesList = useQuery(businessListQuery, "Milano");
+  return (
+    <div className={app}>
+      {pipe(
+        businessesList,
+        fold(
+          () => <>Loading...</>,
+          () => <>An error occurred :(</>,
+          (businesses) => <BusinessList businesses={businesses} />
+        )
+      )}
+    </div>
+  );
 }
 
 export default App;
